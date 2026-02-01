af391b635df6cc503b36c9189a0e523489e58ca2
raulavilesrodriguez <finbravilesr@gmail.com>
2026-02-01 18:39:13 -0500

    working in Screen Home Provider UI

--- feature/home/src/main/java/com/example/feature/home/ui/HomeScreenProvider.kt
+++ feature/home/src/main/java/com/example/feature/home/ui/HomeScreenProvider.kt
@@ -1,6 +1,7 @@
 package com.example.feature.home.ui
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,35 +14,46 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.GridItemSpan
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBalanceWallet
+import androidx.compose.material.icons.filled.Alarm
+import androidx.compose.material.icons.filled.Block
+import androidx.compose.material.icons.filled.BuildCircle
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.People
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.DoneOutline
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.avilesrodriguez.domain.model.referral.ReferralMetrics
 import com.avilesrodriguez.domain.model.user.UserData
+import com.avilesrodriguez.domain.model.user.UserType
 import com.avilesrodriguez.presentation.R
 import com.avilesrodriguez.presentation.avatar.Avatar
 import com.avilesrodriguez.presentation.composables.SearchFieldBasic
 import com.avilesrodriguez.presentation.composables.StatItem
+import com.avilesrodriguez.presentation.details.DetailMetricItem
+import com.avilesrodriguez.presentation.fakeData.userProvider
 import com.example.feature.home.models.UserAndReferralMetrics
 
 @Composable
@@ -56,20 +68,18 @@ fun HomeScreenProvider(
 ){
     val provider = user as UserData.Provider
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
+    LazyColumn(
         modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(12.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        item(span = { GridItemSpan(maxLineSpan) }) {
+        item() {
             BalanceCardProvider(
                 totalReferrals = referralMetricsProvider.totalReferrals.toString(),
                 referralsConversion = provider.referralsConversion?:"0.00"
             )
         }
-        item(span = { GridItemSpan(maxLineSpan) }) {
+        item() {
             Row(
                 modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
                 horizontalArrangement = Arrangement.spacedBy(12.dp)
@@ -109,7 +119,7 @@ fun HomeScreenProvider(
                 )
             }
         }
-        item(span = { GridItemSpan(maxLineSpan)}){
+        item(){
             Column(modifier = Modifier.padding(vertical = 8.dp)) {
                 Text(
                     text = stringResource(R.string.search_referrers),
@@ -126,7 +136,7 @@ fun HomeScreenProvider(
             }
         }
         if (isLoading) {
-            item(span = { GridItemSpan(maxLineSpan) }) {
+            item() {
                 Box(Modifier
                     .fillMaxWidth()
                     .height(200.dp), contentAlignment = Alignment.Center) {
@@ -135,7 +145,7 @@ fun HomeScreenProvider(
             }
         } else {
             items(usersAndMetrics){ client ->
-                ClientCard(clientMetrics = client, onClientClick = onUserClick)
+                ClientRow(clientMetrics = client, onClientClick = onUserClick)
             }
         }
     }
@@ -176,6 +186,88 @@ private fun BalanceCardProvider(totalReferrals: String, referralsConversion: Str
                 fontWeight = FontWeight.Bold,
                 color= MaterialTheme.colorScheme.onPrimary
             )
+        }
+    }
+}
+
+@Composable
+fun ClientRow(clientMetrics: UserAndReferralMetrics, onClientClick: (String) -> Unit){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(12.dp))
+            .clickable{onClientClick(clientMetrics.user.uid)}
+            .background(MaterialTheme.colorScheme.surfaceVariant)
+            .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start
+        ){
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding( 4.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ){
+                Avatar(
+                    photoUri = clientMetrics.user.photoUrl,
+                    size = 40.dp
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = clientMetrics.user.name ?:"",
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                DetailMetricItem(
+                    icon = Icons.Default.People,
+                    value = clientMetrics.referralMetrics.totalReferrals.toString(),
+                    label = stringResource(R.string.total_referrals)
+                )
+            }
+            HorizontalDivider(
+                modifier = Modifier.padding(vertical = 8.dp),
+                thickness = 1.dp,
+                color = MaterialTheme.colorScheme.outline.copy(alpha = 0.3f)
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding( 4.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ){
+                DetailMetricItem(
+                    icon = Icons.Default.Alarm,
+                    value = clientMetrics.referralMetrics.pendingReferrals.toString(),
+                    label = stringResource(R.string.pending)
+                )
+                VerticalDivider(modifier = Modifier.height(40.dp))
+                DetailMetricItem(
+                    icon = Icons.Default.CheckCircle,
+                    value = clientMetrics.referralMetrics.paidReferrals.toString(),
+                    label = stringResource(R.string.paid)
+                )
+                VerticalDivider(modifier = Modifier.height(40.dp))
+                DetailMetricItem(
+                    icon = Icons.Default.BuildCircle,
+                    value = clientMetrics.referralMetrics.processingReferrals.toString(),
+                    label = stringResource(R.string.processing)
+                )
+                VerticalDivider(modifier = Modifier.height(40.dp))
+                DetailMetricItem(
+                    icon = Icons.Default.Block,
+                    value = clientMetrics.referralMetrics.rejectedReferrals.toString(),
+                    label = stringResource(R.string.rejected)
+                )
+            }
         }
     }
 }
@@ -259,3 +351,132 @@ private fun InfoMetric(@DrawableRes icon: Int, title: String, value: String, col
         }
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenProviderPreview(){
+    MaterialTheme {
+        HomeScreenProvider(
+            user = userProvider,
+            isLoading = false,
+            searchText = "",
+            updateSearchText = {},
+            referralMetricsProvider = ReferralMetrics(
+                totalReferrals = 20,
+                pendingReferrals = 5,
+                processingReferrals = 3,
+                rejectedReferrals = 2,
+                paidReferrals = 10
+            ),
+            onUserClick = {},
+            usersAndMetrics = generateFakeUserAndReferralMetrics()
+        )
+    }
+}
+
+private fun generateFakeUserAndReferralMetrics(): List<UserAndReferralMetrics> = listOf(
+    UserAndReferralMetrics(
+        user = UserData.Client(
+            uid = "1",
+            isActive = true,
+            name = "Britny Muelas",
+            email = "britny@gmail.com",
+            photoUrl = "https://i.pravatar.cc/150?u=12",
+            type = UserType.CLIENT,
+            nameLowercase = "britny muelas",
+            identityCard = "1098765432",
+            countNumberPay = "12223440455",
+            bankName = "Produbanco",
+            accountType = "Ahorros",
+            moneyEarned = "1000",
+            moneyReceived = "950",
+            totalReferrals = 10,
+            pendingPayments = 1
+        ),
+        referralMetrics = ReferralMetrics(
+            totalReferrals = 5,
+            pendingReferrals = 1,
+            processingReferrals = 1,
+            rejectedReferrals = 2,
+            paidReferrals = 1
+        )
+    ),
+    UserAndReferralMetrics(
+        user = UserData.Client(
+            uid = "1",
+            isActive = true,
+            name = "Liz Aura",
+            email = "lizaura@gmail.com",
+            photoUrl = "https://i.pravatar.cc/150?u=20",
+            type = UserType.CLIENT,
+            nameLowercase = "liz aura",
+            identityCard = "1249765432",
+            countNumberPay = "12223440466",
+            bankName = "Pichincha",
+            accountType = "Ahorros",
+            moneyEarned = "1500",
+            moneyReceived = "1200",
+            totalReferrals = 10,
+            pendingPayments = 3
+        ),
+        referralMetrics = ReferralMetrics(
+            totalReferrals = 5,
+            pendingReferrals = 2,
+            processingReferrals = 0,
+            rejectedReferrals = 1,
+            paidReferrals = 2
+        )
+    ),
+    UserAndReferralMetrics(
+        user = UserData.Client(
+            uid = "1",
+            isActive = true,
+            name = "Silvana Murillo",
+            email = "silvi@gmail.com",
+            photoUrl = "https://i.pravatar.cc/150?u=22",
+            type = UserType.CLIENT,
+            nameLowercase = "silvana murillo",
+            identityCard = "0015765439",
+            countNumberPay = "12223440498",
+            bankName = "Banco de Guayaquil",
+            accountType = "Ahorros",
+            moneyEarned = "4000",
+            moneyReceived = "3500",
+            totalReferrals = 15,
+            pendingPayments = 4
+        ),
+        referralMetrics = ReferralMetrics(
+            totalReferrals = 5,
+            pendingReferrals = 1,
+            processingReferrals = 1,
+            rejectedReferrals = 0,
+            paidReferrals = 3
+        )
+    ),
+    UserAndReferralMetrics(
+        user = UserData.Client(
+            uid = "1",
+            isActive = true,
+            name = "Julio Cuvero",
+            email = "julio.cuvero@gmail.com",
+            photoUrl = "https://i.pravatar.cc/150?u=23",
+            type = UserType.CLIENT,
+            nameLowercase = "julio cuvero",
+            identityCard = "1495765430",
+            countNumberPay = "10273440456",
+            bankName = "Austro",
+            accountType = "Ahorros",
+            moneyEarned = "2000",
+            moneyReceived = "1700",
+            totalReferrals = 12,
+            pendingPayments = 2
+        ),
+        referralMetrics = ReferralMetrics(
+            totalReferrals = 5,
+            pendingReferrals = 2,
+            processingReferrals = 0,
+            rejectedReferrals = 2,
+            paidReferrals = 1
+        )
+    ),
+)
