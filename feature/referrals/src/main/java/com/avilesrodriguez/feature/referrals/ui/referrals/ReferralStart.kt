0720292dff98cecef44c4d96490f24c225d6b4d0
Raul Aviles <finbravilesr@gmail.com>
2026-03-12 19:10:18 -0500

    add icons in splash screens in referralStart.kt and EditSplash.kt

--- feature/referrals/src/main/java/com/avilesrodriguez/feature/referrals/ui/referrals/ReferralStart.kt
+++ feature/referrals/src/main/java/com/avilesrodriguez/feature/referrals/ui/referrals/ReferralStart.kt
@@ -2,13 +2,19 @@ package com.avilesrodriguez.feature.referrals.ui.referrals
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.avilesrodriguez.presentation.R
 
 @Composable
@@ -18,6 +24,15 @@ fun ReferralStar(){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Icon(
+            painter = painterResource(R.drawable.person_pin),
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .size(64.dp),
+            tint = MaterialTheme.colorScheme.primary,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
         Text(
             text = stringResource(R.string.select_referred),
             modifier = Modifier.align(Alignment.CenterHorizontally),
