058db731c51b03118f0a6d7261fb8a73b53cd2e7
Raul Aviles <finbravilesr@gmail.com>
2026-04-18 19:52:24 -0500

    developing webhook to connect with pyphone

---
+++ core/domain/src/main/java/com/avilesrodriguez/domain/model/payments/Payments.kt
@@ -0,0 +1,10 @@
+package com.avilesrodriguez.domain.model.payments
+
+data class Payments(
+    val id: String = "",
+    val uid: String = "",
+    val amount: Double = 0.0,
+    val authorizationCode: String = "",
+    val date: Long = System.currentTimeMillis(),
+    val status: String
+)
